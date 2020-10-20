7ec15116d8c7c6a1e8ae449f1ecef5e2ae444709
weimenghua <weimenghua@tc-lawtech.com>
2020-10-20 10:05:32 +0800

    数组排序

---
+++ learn3/sort.py
@@ -0,0 +1,34 @@
+dbname_list=(
+"ats",
+"amp",
+"aut",
+"bcs",
+"bsf",
+"bss",
+"cfg",
+"css",
+"ctr",
+"cts",
+"dst",
+"ell",
+"irs",
+"jos",
+"mds",
+"mfs",
+"mqa",
+"nps",
+"ntf",
+"pts",
+"rcf",
+"res",
+"tcs",
+"uts",
+"gow",
+"tcb",
+)
+
+
+array_dbname_list = sorted(dbname_list)
+print(array_dbname_list)
+
+array_dbname_list = ['amp', 'ats', 'aut', 'bcs', 'bsf', 'bss', 'cfg', 'css', 'ctr', 'cts', 'dst', 'ell', 'gow', 'irs', 'jos', 'mds', 'mfs', 'mqa', 'nps', 'ntf', 'pts', 'rcf', 'res', 'tcb', 'tcs', 'uts']
